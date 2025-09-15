1b1e4d77b51987c66445d9ba2d860b93538bdc40
brunodeathayde <49995744+brunodeathayde@users.noreply.github.com>
2025-09-15 11:11:37 -0300

    Update next_fit.py

--- bin packing 1D/next_fit.py
+++ bin packing 1D/next_fit.py
@@ -11,26 +11,3 @@ def next_fit(itens, capacidade):
             free_bin = capacidade - item
 
     return bins
-
-
-
-
-"""
-Definir capacidade_da_caixa ← C
-Inicializar lista_de_itens ← [i₁, i₂, ..., iₙ]
-Inicializar número_de_caixas ← 1
-Inicializar espaço_disponível ← C
-
-PARA cada item em lista_de_itens FAÇA:
-    SE item ≤ espaço_disponível ENTÃO
-        Colocar item na caixa atual
-        Atualizar espaço_disponível ← espaço_disponível - item
-    SENÃO
-        Abrir nova caixa
-        número_de_caixas ← número_de_caixas + 1
-        espaço_disponível ← C - item
-        Colocar item na nova caixa
-FIM PARA
-
-Exibir número_de_caixas
-"""
