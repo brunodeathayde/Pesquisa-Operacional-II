a141eb260c351f1261d5a7694c246adae5ab9528
brunodeathayde <49995744+brunodeathayde@users.noreply.github.com>
2025-09-30 17:55:00 -0300

    Update gillet_miller.py

--- vehicle routing problem/gillet_miller.py
+++ vehicle routing problem/gillet_miller.py
@@ -9,18 +9,12 @@ import numpy
 def gillet_miller(n, P):
     s = [0 for i in range(n)]
     for i in range(n):
-        # Evita divisão por zero e calcula ângulo polar
         x, y = P[i][0], P[i][1]
-        #s[i] = numpy.arctan2(y, x)
-        #s[i] = numpy.degrees(numpy.arctan2(y, x))
-        #s[i] = round(numpy.degrees(numpy.arctan2(y, x)), 2)
         s[i] = numpy.degrees(numpy.arctan2(y, x)) % 360
 
 
     print(", ".join(f"{valor:.1f}" for valor in s))
  
-
-
     route = [i + 1 for i in sorted(range(n), key=lambda k: s[k])]
     return route
 
