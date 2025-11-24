e0677ca8c6f21e9d39e05dd925aa7833368d860c
brunodeathayde <49995744+brunodeathayde@users.noreply.github.com>
2025-11-24 12:34:26 -0300

    Update objective_function_tsp.py

--- Metaheuristics/Simulated Annealing/TSP/objective_function_tsp.py
+++ Metaheuristics/Simulated Annealing/TSP/objective_function_tsp.py
@@ -1,8 +1,6 @@
-def objective_function_tsp(n,s,D):
-    d = 0
+def objective_function_tsp(n, route, D):
+    dist = 0
     for i in range(n-1):
-        d += D[s[i]][s[i+1]]
-
-    d+=D[s[n-1]][s[0]]
-
-    return(d)
+        dist += D[route[i]][route[i+1]]
+    dist += D[route[-1]][route[0]]  # fecha o ciclo
+    return dist
